93b900fd504551a2c119a5d4fa2595ca5d929c40
Владислав Василенко <vasilekx8@yandex.ru>
2022-03-21 14:50:33 +0300

    Update files

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -51,8 +51,7 @@ class FollowViewSet(CreateListViewSet):
     search_fields = ('following__username',)
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.request.user)
-        return user.follower.all()
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
